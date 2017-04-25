f6b225cbf7ab992366270ef645ad22eaf2694653
daidai <13022779332@163.com>
2017-04-25 10:14:46 +0800

    dd

--- app/Transformers/VisitorTransformer.php
+++ app/Transformers/VisitorTransformer.php
@@ -4,20 +4,18 @@ namespace App\Transformers;
 
 use App\Visitor;
 use League\Fractal\TransformerAbstract;
-use Carbon\Carbon;
 
 class VisitorTransformer extends TransformerAbstract
 {
     public function transform(Visitor $visitor)
     {
-        $date = Carbon::now();
         return [
             'id'            => $visitor->id,
             'article'       => [ 'title' => isset($visitor->article) ? $visitor->article->title : 'null' ],
             'ip'            => $visitor->ip,
             'country'       => $visitor->country,
             'clicks'        => $visitor->clicks,
-            'created_at'    => $visitor->created_at->$date,
+            'created_at'    => $visitor->created_at->toDateTimeString(),
         ];
     }
 }
