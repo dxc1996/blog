7183c93ec9f33541129788da821637ef0a7868eb
daidai <13022779332@163.com>
2017-04-26 17:03:22 +0800

    dd

--- config/blog.php
+++ config/blog.php
@@ -13,8 +13,8 @@ return [
 
     // Meta
     'meta' => [
-        'keywords' => 'HalfWays',
-        'description' => 'HalfWays'
+        'keywords' => 'Halfways',
+        'description' => 'Halfways'
     ],
 
     // Social Share
@@ -33,7 +33,7 @@ return [
 
     // Article Page
     'article' => [
-        'title'       => 'HalfWays',
+        'title'       => 'Halfways',
         'description' => '',
         'number'      => 15,
         'sort'        => 'desc',
